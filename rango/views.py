41c2e4295f632c5b541a9dd065f3e8c5d2a62509
Zishan Ahmad <zishanahmad@outlook.com>
2015-06-20 05:19:37 +0530

    templates now extend from the base template

--- rango/views.py
+++ rango/views.py
@@ -15,6 +15,9 @@ def index(request):
     }
     return render(request, 'rango/index.html', context_dict)
 
+def about(request):
+    return render(request, 'rango/about.html', {})
+
 def category(request, category_name_slug):
     context_dict = {}
     try:
